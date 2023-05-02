e9b6653c92720454ad948bd06958069d383cd9fc
Luciano Silva <legostr@gmail.com>
2023-05-01 21:35:24 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "./App.css"
+import Header from "./components/header/Header"
+
+const App = () => {
+  console.log("O componente App está sendo renderizado.")
+  return (
+    <>
+      <Header />
+    </>
+    
+  )
+  
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import "./header.css"
+
+const Header = () => {
+  console.log("O componente App está sendo renderizado.")
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
